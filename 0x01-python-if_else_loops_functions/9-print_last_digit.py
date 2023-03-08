2a8601822bd1e38a9986cfde2a5c62367d6fff2b
Jacobs Agyei <57623419+jacobsqxv@users.noreply.github.com>
2023-03-08 22:59:50 +0000

    print last digit

---
+++ 0x01-python-if_else_loops_functions/9-print_last_digit.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def print_last_digit(number):
+    if number >= 0:
+        last = number % 10
+    else:
+        temp = number * -1
+        last = (temp % 10)
+    print('{:d}'.format(last), end="")
+    return last
